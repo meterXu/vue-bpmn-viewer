fabcc3b7f5301248039486d5a30817c663965c6b
Kobeoneone <junshidehuitou@163.com>
2021-07-15 08:58:00 +0800

    【提交内容】 修改驳回的时候加入参数variables 【提交人】 高强

--- sipsd-flowable-parent/sipsd-flowable-api/src/main/java/com/sipsd/flow/vo/flowable/PreBackTaskVo.java
+++ sipsd-flowable-parent/sipsd-flowable-api/src/main/java/com/sipsd/flow/vo/flowable/PreBackTaskVo.java
@@ -2,16 +2,16 @@ package com.sipsd.flow.vo.flowable;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Description: 流程执行过程中的基本参数VO
  * @Author: chengtg
  */
-@Data
 @ApiModel("返回原节点流程执行过程中的基本参数VO")
 public class PreBackTaskVo implements Serializable {
 	/**
@@ -39,4 +39,49 @@ public class PreBackTaskVo implements Serializable {
 	@ApiModelProperty(value = "流程实例的id", required = true)
 	@NotBlank(message = "流程实例的id 必填")
 	private String processInstanceId;
+
+
+	/**
+	 * 任务参数 选填
+	 */
+	@ApiModelProperty("任务参数")
+	private Map<String, Object> variables = new HashMap<String, Object>();
+
+	public Map<String, Object> getVariables() {
+		return variables;
+	}
+
+	public void setVariables(Map<String, Object> variables) {
+		this.variables = variables;
+	}
+
+	public String getTaskId()
+	{
+		return taskId;
+	}
+
+	public void setTaskId(String taskId)
+	{
+		this.taskId = taskId;
+	}
+
+	public String getMessage()
+	{
+		return message;
+	}
+
+	public void setMessage(String message)
+	{
+		this.message = message;
+	}
+
+	public String getProcessInstanceId()
+	{
+		return processInstanceId;
+	}
+
+	public void setProcessInstanceId(String processInstanceId)
+	{
+		this.processInstanceId = processInstanceId;
+	}
 }
